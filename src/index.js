292e1c072499d745e718e75fe984092ad83422bd
Nastishka <anastasiya.matronkina.com>
2020-03-01 22:20:55 +0300

    feat: implement max, min, avg functions for working with arrays

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    return (Array.isArray(array) && array.length > 0) ?
+        Math.min(...array) : 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    return (Array.isArray(array) && array.length > 0) ?
+        Math.max(...array) : 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    let avg = 0;
+    if (Array.isArray(array) && array.length > 0) {
+        let sum = array.reduce((total, item) => total + item);
+        avg = sum / array.length;
+    }
+    return avg;
 }
